db2c30cdfc91dcfb60dd28ea7f69df4b29fa65b6
hideaki51 <hs.wgp.46@gmail.com>
2024-04-18 11:23:06 +0900

    Navbarコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListingsComponent } from './product-listings/product-listings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductListingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-listings/product-listings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListingsComponent {
+
+}
